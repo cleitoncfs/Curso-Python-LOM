38140ff81d47e27b721eaf66d5dbcae4d303a1df
Cleiton Santos <cleitoncfs@gmail.com>
2024-10-31 16:48:52 +0000

    Update

--- aula32.py
+++ aula32.py
@@ -3,31 +3,24 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-# entrada = input('Digite um número: ')
+entrada = input('Digite um número: ')
 
-# if entrada.isdigit():
-#     entrada_int = int(entrada)
-#     par_impar = entrada_int % 2 == 0
-#     par_impar_texto = 'ímpar'
+if entrada.isdigit():
+    entrada_int = int(entrada)
+    par_impar_texto = 'par' if entrada_int % 2 == 0 else 'ímpar'
+    print(f'O número {entrada_int} é {par_impar_texto}')
+else:
+    try:
+        entrada_float = float(entrada)
+        if entrada_float.is_integer():
+            entrada_int = int(entrada_float)
+            par_impar_texto = 'par' if entrada_int % 2 == 0 else 'ímpar'
+            print(f'O número {entrada_int} é {par_impar_texto}')
+        else:
+            print('Você não digitou um número inteiro')
+    except ValueError:
+        print('Você não digitou um número inteiro')
 
-#     if par_impar:
-#         par_impar_texto = 'par'
-
-#     print(f'O número {entrada_int} é {par_impar_texto}')
-# else:
-#     print('Você não digitou um número inteiro')
-
-# try:
-#     entrada_int = float(entrada)
-#     par_impar = entrada_int % 2 == 0
-#     par_impar_texto = 'ímpar'
-
-#     if par_impar:
-#         par_impar_texto = 'par'
-
-#     print(f'O número {entrada_int} é {par_impar_texto}')
-# except:
-#     print('Você não digitou um número inteiro')
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
@@ -53,15 +46,15 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
-nome = input('Digite seu nome: ')
-tamanho_nome = len(nome)
+# nome = input('Digite seu nome: ')
+# tamanho_nome = len(nome)
 
-if tamanho_nome > 1:
-    if tamanho_nome <= 4:
-        print('Seu nome é curto')
-    elif tamanho_nome >= 5 and tamanho_nome <= 6:
-        print('Seu nome é normal')
-    else:
-        print('Seu nome é muito grande')
-else:
-    print('Digite mais de uma letra.')
+# if tamanho_nome > 1:
+#     if tamanho_nome <= 4:
+#         print('Seu nome é curto')
+#     elif tamanho_nome >= 5 and tamanho_nome <= 6:
+#         print('Seu nome é normal')
+#     else:
+#         print('Seu nome é muito grande')
+# else:
+#     print('Digite mais de uma letra.')
