11484114d1ebf0d6e2bc420308d3b3740b4df75d
Cleiton Santos <cleitoncfs@gmail.com>
2024-10-31 18:23:06 +0000

    Update

--- aula32.py
+++ aula32.py
@@ -26,21 +26,21 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
-# entrada = input('Digite a hora em números inteiros: ')
+entrada = input('Digite a hora em números inteiros: ')
 
-# try:
-#     hora = int(entrada)
+try:
+    hora = int(entrada)
 
-#     if hora >= 0 and hora <= 11:
-#         print('Bom dia')
-#     elif hora >= 12 and hora <= 17:
-#         print('Boa tarde')
-#     elif hora >= 18 and hora <= 23:
-#         print('Boa noite')
-#     else:
-#         print('Não conheço essa hora')
-# except:
-#     print('Por favor, digite apenas números inteiros')
+    if hora >= 0 and hora <= 11:
+        print('Bom dia')
+    elif hora >= 12 and hora <= 17:
+        print('Boa tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Boa noite')
+    else:
+        print('Não conheço essa hora')
+except:
+    print('Por favor, digite apenas números inteiros')
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
